edcd60c9ca1292c920ab8270866d279e2f937c51
Rahul Prasanna Kumar <rprasan@clemson.edu>
2023-06-24 20:47:36 -0500

    Delete DQN/Results directory

--- DQN/Results/frogger_env/__init__.py
+++
@@ -1,4 +0,0 @@
-import os
-os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = '1'
-# Import the envs module so that envs register themselves
-import frogger_env.envs
--- DQN/Results/frogger_env/envs/__init__.py
+++
@@ -1,2 +0,0 @@
-from frogger_env.envs.crossing_env import *
-from frogger_env.envs.crossing_env_v1 import *
